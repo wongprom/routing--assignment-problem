2ea45e01d3795fa4a5addf9e6d0a6970b6597770
wongprom <jimmybackstrom@hotmail.com>
2019-11-11 15:22:51 +0100

    Done 4, dynamic loading course.

--- src/containers/Course/Course.js
+++ src/containers/Course/Course.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 
 class Course extends Component {
-    render () {
+    render() {
         return (
             <div>
                 <h1>_COURSE_TITLE_</h1>
-                <p>You selected the Course with ID: _ID_</p>
+                <p>You selected the Course with ID:{this.props.match.params.id}</p>
             </div>
         );
     }
