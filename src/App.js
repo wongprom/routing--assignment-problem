fc4ea639093bd17ed606203ac59a3415084ade51
wongprom <jimmybackstrom@hotmail.com>
2019-11-11 14:09:37 +0100

    Done with 3. Using <Link>.

--- src/App.js
+++ src/App.js
@@ -40,16 +40,18 @@ class App extends Component {
           <Route path="/Users" component={Users} />
           <Route path="/Courses" component={Courses} />
 
-          <ol style={{ textAlign: 'left' }}>
-            <li style={{ textDecoration: 'line-through' }}>Add Routes to load "Users" and "Courses" on different pages (by entering a URL, without Links)</li>
-            <li style={{ textDecoration: 'line-through' }}>Add a simple navigation with two links => One leading to "Users", one leading to "Courses"</li>
-            <li>Make the courses in "Courses" clickable by adding a link and load the "Course" component in the place of "Courses" (without passing any data for now)</li>
-            <li>Pass the course ID to the "Course" page and output it there</li>
-            <li>Pass the course title to the "Course" page - pass it as a param or score bonus points by passing it as query params (you need to manually parse them though!)</li>
-            <li>Load the "Course" component as a nested component of "Courses"</li>
-            <li>Add a 404 error page and render it for any unknown routes</li>
-            <li>Redirect requests to /all-courses to /courses (=> Your "Courses" page)</li>
-          </ol>
+          <h3>
+            <ol style={{ textAlign: 'left' }}>
+              <li style={{ textDecoration: 'line-through' }}>Add Routes to load "Users" and "Courses" on different pages (by entering a URL, without Links)</li>
+              <li style={{ textDecoration: 'line-through' }}>Add a simple navigation with two links => One leading to "Users", one leading to "Courses"</li>
+              <li style={{ textDecoration: 'line-through' }}>Make the courses in "Courses" clickable by adding a link and load the "Course" component in the place of "Courses" (without passing any data for now)</li>
+              <li>Pass the course ID to the "Course" page and output it there</li>
+              <li>Pass the course title to the "Course" page - pass it as a param or score bonus points by passing it as query params (you need to manually parse them though!)</li>
+              <li>Load the "Course" component as a nested component of "Courses"</li>
+              <li>Add a 404 error page and render it for any unknown routes</li>
+              <li>Redirect requests to /all-courses to /courses (=> Your "Courses" page)</li>
+            </ol>
+          </h3>
         </div>
       </BrowserRouter>
 
--- src/containers/Courses/Courses.js
+++ src/containers/Courses/Courses.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import { Link } from "react-router-dom";
 import './Courses.css';
 
 class Courses extends Component {
@@ -11,17 +11,24 @@ class Courses extends Component {
         ]
     }
 
-    render () {
+    render() {
         return (
             <div>
                 <h1>Amazing Udemy Courses</h1>
+
                 <section className="Courses">
                     {
-                        this.state.courses.map( course => {
-                            return <article className="Course" key={course.id}>{course.title}</article>;
-                        } )
+                        this.state.courses.map(course => {
+                            return (
+                                <Link to={"/course"}>
+                                    <article className="Course" key={course.id}>{course.title}</article>
+                                </Link>
+                            );
+                        })
                     }
                 </section>
+
+
             </div>
         );
     }
